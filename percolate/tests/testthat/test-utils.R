0eb79097c09a850b44865ffda78abadfdcff4b94
gray <shinjoohyung25@gmail.com>
2019-07-31 18:12:14 -0400

    add test-utils.R

---
+++ percolate/tests/testthat/test-utils.R
@@ -0,0 +1,33 @@
+library(assertthat)
+library(testthat)
+
+test_that("default matrix is 5 by 5 with only 0, 1, 2", {
+  mat <- generate_board_mat()
+  expect_equal(dim(mat)[1], 5)
+  expect_equal(dim(mat)[2], 5)
+  expect_equal(sum(mat %in% c(0, 1, 2)), 25)
+})
+
+test_that("check with matrix n = 6", {
+  mat <- generate_board_mat(n = 6)
+  expect_equal(dim(mat)[1], 6)
+  expect_equal(dim(mat)[2], 6)
+  expect_equal(sum(mat %in% c(0, 1, 2)), 36)
+})
+
+test_that("p = 0 gives a board with all 1", {
+  mat <- generate_board_mat(p = 0)
+  expect_equal(sum(mat == 1), 25)
+})
+
+test_that("p = 1 gives a board with all 0", {
+  mat <- generate_board_mat(p = 1)
+  expect_equal(sum(mat), 0)
+})
+
+test_that("check possible errors", {
+  expect_error(generate_board_mat(n = c(1,2)))
+  expect_error(generate_board_mat(n = "asdf"))
+  expect_error(generate_board_mat(n = 5.4))
+  expect_error(generate_board_mat(n = -5))
+})
