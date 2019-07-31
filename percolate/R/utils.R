b681634c9c0735e4bfbac8ec1ce8a077eba96be4
gray <shinjoohyung25@gmail.com>
2019-07-31 17:55:02 -0400

    fix errors - block is 0, non-block is 1

--- percolate/R/utils.R
+++ percolate/R/utils.R
@@ -1,20 +1,24 @@
+library(assertthat)
+library(testthat)
+library(tidyverse)
+
 #' Title generate_board_mat
 #'
 #' @param n a positive integer denote the size of the board
-#' @param p a number between 0 and 1 that denotes the fraction of the `n^2` squares are blocked 
+#' @param p a number between 0 and 1 that denotes the fraction of the `n^2` squares are blocked
 #'
 #' @return matrix with random (0, 1) (n^2 size with n^2 * p numbers of blocked(1), rest (0))
 #' @export
 #'
-#' @examples
+#' @examples 0 is blocked(black) 1 is non-blocked(white)
 generate_board_mat <- function(n = 5, p = 0.25) {
   assert_that(is.numeric(n))
   assert_that(is.numeric(p))
   assert_that(n > 0)
   assert_that(p >= 0 & p <= 1)
-  
+
   blocked <- floor(n^2 * p)
   non_blocked <- n^2 - blocked
-  gen_blocks <- sample(c(rep(1, blocked), rep(0, non_blocked)))
+  gen_blocks <- sample(c(rep(0, blocked), rep(1, non_blocked)))
   return(matrix(gen_blocks, nrow = n, ncol = n))
 }
