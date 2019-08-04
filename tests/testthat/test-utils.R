2fe1bde2ce8bc140e0272aec20f875d3c6077147
gray <shinjoohyung25@gmail.com>
2019-08-04 19:25:47 -0400

    fix test-utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,24 +2,24 @@ test_that("default matrix is 5 by 5 with only 0, 1, 2", {
   mat <- generate_board_mat()
   expect_equal(dim(mat)[1], 5)
   expect_equal(dim(mat)[2], 5)
-  expect_equal(sum(mat %in% c(0, 1, 2)), 25)
+  expect_true(all(mat %in% c(0, 1, 2)))
 })
 
 test_that("check with matrix n = 6", {
   mat <- generate_board_mat(n = 6)
   expect_equal(dim(mat)[1], 6)
   expect_equal(dim(mat)[2], 6)
-  expect_equal(sum(mat %in% c(0, 1, 2)), 36)
+  expect_true(all(mat %in% c(0, 1, 2)))
 })
 
 test_that("p = 0 gives a board with all 1", {
   mat <- generate_board_mat(p = 0)
-  expect_equal(sum(mat == 1), 25)
+  expect_true(all(mat == 1))
 })
 
 test_that("p = 1 gives a board with all 0", {
   mat <- generate_board_mat(p = 1)
-  expect_equal(sum(mat), 0)
+  expect_true(all(mat == 0))
 })
 
 test_that("check possible errors", {
@@ -30,8 +30,11 @@ test_that("check possible errors", {
 })
 
 test_that("check is_valid", {
+  ## not matrix
   expect_error(is_valid(mat = c(1,2,3)))
+  ## not squre matrix
   expect_error(is_valid(mat = matrix(ncol = 5, nrow = 11)))
+  ## not 0, 1, 2
   expect_error(is_valid(mat = matrix(1:25, nrow = 5, ncol = 5)))
 })
 
